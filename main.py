1c26a8e06a9fdb322d413dc09d030682d39e01ee
Krekhov Nikolai <krekhov.na@phystech.edu>
2022-10-04 05:41:23 +0300

    fix bages in dka

--- main.py
+++ main.py
@@ -2,12 +2,6 @@
 from collections import defaultdict
 from collections import deque
 import sys
-
-
-def get_state(str):
-    return str.split(',')[0]
-
-
 class automaton:
 
     def __init__(self, path: str):
@@ -82,12 +76,16 @@ class automaton:
             lines = lines[i:]
         return transitions
 
-    def find_eps_close(self, vertions):
-        reachable_vertions = vertions
-        for vert in vertions:
-            reachable_from_vert = self.eps_reachable[str(vert)]
-            reachable_vertions = set.union(reachable_from_vert, reachable_from_vert)
-        return reachable_vertions
+    def relax_eps(self):
+        for vert in self.eps_reachable:
+            for to_EPS in self.eps_reachable[vert]:
+                if to_EPS in self.terminal:
+                    self.terminal.append(vert)
+                for to_state in self.transitions[to_EPS]:
+                    to, symbol = to_state.split(",")
+                    self.transitions[f"{vert}"].add(f"{to},{symbol}")
+                self.transitions[vert].discard(f"{to_EPS},EPS")
+        self.terminal = set(self.terminal)
 
     def extend_vertions(self, array, symbol):
         ans = set()
@@ -98,30 +96,30 @@ class automaton:
         return sorted(ans)
 
     def to_dka(self):
+        self.relax_eps()
         new_terminal = set()
         q = deque()
         q.append(self.start)
         new_states = []
         new_transitions = defaultdict(set)
         while len(q) != 0:
+            flag = False
             current_array_for_symbol = q.popleft()
             set_current_array_for_symbol = set(current_array_for_symbol.split(':'))
-            set_current_array_for_symbol = set.union(set_current_array_for_symbol,
-                                                     self.find_eps_close(current_array_for_symbol))
-            for vert in current_array_for_symbol:
-                if vert in self.terminal or vert in new_terminal:
-                    new_terminal.add(current_array_for_symbol)
-                    break
             for symbol in self.alphabet:
                 new_curr_states = self.extend_vertions(set_current_array_for_symbol, symbol)
                 new_curr_states = self.format(new_curr_states)
-
-                if not (new_curr_states in new_states):
-                    if new_curr_states != "":
-                        q.append(new_curr_states)
-                    new_states.append(self.format(set_current_array_for_symbol))
+                if (new_curr_states != "" and not (new_curr_states in new_states)):
+                    q.append(new_curr_states)
+                    flag = True
                 if new_curr_states != "":
+                    new_states.append(self.format(set_current_array_for_symbol))
                     new_transitions[current_array_for_symbol].add(f"{new_curr_states},{symbol}")
+                    if flag:
+                        for vert in set_current_array_for_symbol:
+                            if vert in self.terminal or (vert in new_terminal):
+                                new_terminal.add(current_array_for_symbol)
+                                break
         self.terminal = new_terminal
         self.transitions = new_transitions
         self.states = list(set(new_states))
@@ -232,13 +230,13 @@ class automaton:
         self.transitions = new_transitions
         self.terminal = new_terminal
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: doa2graphviz file")
-    else:
-        a = automaton(sys.argv[1])
-        a.to_dka()
-        a.to_pdka()
-        a.to_min_pdka()
-        a.print_graph()
+#
+# if __name__ == "__main__":
+#     if len(sys.argv) != 2:
+#         print("Usage: doa2graphviz file")
+#     else:
+#         a = automaton(sys.argv[1])
+#         a.to_dka()
+#         a.to_pdka()
+#         a.to_min_pdka()
+#         a.print_graph()
